b3c2293533e9632971931e5d9d15bc653e621ab9
Aleksandra Szychlinska <szychlinska@gmail.com>
2021-05-05 19:50:49 +0200

    nav menu and modals working

--- js/main.js
+++ js/main.js
@@ -19,11 +19,26 @@ window.addEventListener('load', function(){
     preloader.classList.add('hide-preloader')
 })
 
-//modal 
+//contact-modal 
+const openContact = document.getElementById('contact-open')
+const contactClose = document.getElementById('contact-close')
+const contactModal = document.querySelector('.contact-modal')
 
-const closeBtn = document.querySelector('.close-btn')
-const contactModal = document.querySelector('.modal');
+openContact.addEventListener('click', () => {
+    contactModal.classList.add('open-modal')
+})
+contactClose.addEventListener('click', () => {
+    contactModal.classList.remove('open-modal')
+})
 
-closeBtn.addEventListener('click', () => {
-    contactModal.classList.add('close-modal')
+//services-modal 
+const servicesOpen = document.getElementById('services-open')
+const servicesClose = document.getElementById('services-close')
+const servicesModal = document.querySelector('.services-modal')
+
+servicesOpen.addEventListener('click', () => {
+    servicesModal.classList.add('open-modal')
+})
+servicesClose.addEventListener('click', () => {
+    servicesModal.classList.remove('open-modal')
 })

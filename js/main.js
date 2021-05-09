db3d7a87f291569a012413df28d47bed014a30a1
Aleksandra Szychlinska <szychlinska@gmail.com>
2021-05-09 17:20:30 +0200

    plus staff names minus services

--- js/main.js
+++ js/main.js
@@ -25,25 +25,24 @@ window.addEventListener('load', function(){
 const openContact = document.getElementById('contact-open')
 const contactClose = document.getElementById('contact-close')
 const contactModal = document.querySelector('.contact-modal')
-const homepage = document.querySelector('.homepage-content')
 
 openContact.addEventListener('click', () => {
+    console.log('contact')
     contactModal.classList.add('open-modal')
-    homepage.classList.add('homepage-lessvisible')
 })
 contactClose.addEventListener('click', () => {
     contactModal.classList.remove('open-modal')
-    homepage.classList.remove('homepage-lessvisible')
 })
 
 //services-modal 
-const servicesOpen = document.getElementById('services-open')
-const servicesClose = document.getElementById('services-close')
-const servicesModal = document.querySelector('.services-modal')
+// const servicesOpen = document.getElementById('services-open')
+// const servicesClose = document.getElementById('services-close')
+// const servicesModal = document.querySelector('.services-modal')
 
-servicesOpen.addEventListener('click', () => {
-    servicesModal.classList.add('open-modal')
-})
-servicesClose.addEventListener('click', () => {
-    servicesModal.classList.remove('open-modal')
-})
+// servicesOpen.addEventListener('click', () => {
+//     console.log('services')
+//     servicesModal.classList.add('open-modal')
+// })
+// servicesClose.addEventListener('click', () => {
+//     servicesModal.classList.remove('open-modal')
+// })
